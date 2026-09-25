1eeb275bc62b47aaccb33d981f558ae4d79c065e
barbosajuh <julia.magalhaes@estudante.ifms.edu.br>
2026-09-24 21:07:17 -0400

    Criando middlewares para buscar e filtar tarefas

--- servidor.js
+++ servidor.js
@@ -17,6 +17,44 @@ app.get('/', (req, res) => {
     res.json({Mensagem: 'API de Tarefas no ar!'})
 })
 
+function filtrarTarefas(req, res, next){
+    const {concluida} = req.query
+
+    if(concluida !== undefined){
+        const foiConcluida = concluida === 'true'
+
+        const tarefasConcluidas = tarefas.filter(tarefa => tarefa.concluida === foiConcluida)
+        
+        return res.json(tarefasConcluidas)
+    }
+
+    next()
+}
+
+app.get('/tarefas', filtrarTarefas, (req, res) => {
+    res.json(tarefas)
+})
+
+function buscarTarefa(req, res, next){
+    const id = req.params.id
+    const idTarefa = parseInt(id)
+
+    const tarefa = tarefas.find(t => t.id === parseInt(idTarefa)) 
+
+    if(!tarefa){
+        return res.status(404).json({Error: 'Tarefa não encontrado!'})
+    }
+
+    req.tarefa = tarefa
+
+    next()
+}
+
+app.get('/tarefas/:id', buscarTarefa, (req, res) => {
+    res.json(req.tarefa)
+})
+
+
 app.listen(PORTA, () => {
     console.log(`Servidor rodando em http://localhost:${PORTA}`)
 })
